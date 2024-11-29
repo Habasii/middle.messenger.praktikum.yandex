55272ba1cbe2f4b201bddf8002a29779ab8fb247
Nelly <danmer_2@mail.ru>
2024-11-29 21:47:40 +0300

    Eslint

---
+++ eslint.config.js
@@ -0,0 +1,8 @@
+// eslint.config.js
+export default [
+    {
+        rules: { semi: "error", "prefer-const": "error" },
+        ignores: [".config/*", "/node_modules", "/build", ".github", ".eslintignore"]
+    }
+  ];
+  
